6352429a51bde35b18c7aa698c30930ffdd24227
user2021 HU <user2021@iitu.edu.kz>
2024-10-21 21:13:46 +0500

    qwe

---
+++ cinema_app/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+# Виды полей в Джанго
+# CharField -> string(строка) с ограничением по длине. Длина равна max_length
+# TextField -> string(строка) без ограничения по длине
+# DecimalField -> Не целые числа. Кол-во цифр(всех) равна max_digits, сколько за запятой равно decimal_places
+# PositiveSmallIntegerField -> Целое положительное число до 32767
+# PositiveIntegerField -> Целое положительное число до 2147483647
+# PositiveBigIntegerField -> Целое положительное число до 9223372036854775807
+
+class Film(models.Model):  # Film -> имя таблицы в SQL
+    # id создавать не нужно, Джанго по стандарту создает сам
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    genre = models.CharField(max_length=255)
+    duration = models.PositiveSmallIntegerField()
+    country = models.CharField(max_length=255)
